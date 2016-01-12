7beca775cc7e1696006b26e81712c3082c83c33d
Yigit <yigitsayan@gmail.com>
2016-01-12 17:58:56 +0200

    Vanilla design header has been adjusted

--- themes/custom/hangiclub/lib/main.js
+++ themes/custom/hangiclub/lib/main.js
@@ -1,3 +1,13 @@
 jQuery(document).ready(function($) {
-    
+    $(window).resize( function(){
+            $(".slider").owlCarousel({
+                items: 3,
+                nav: false,
+                responsive: true,
+                lazyLoad: true,
+                autoPlay : 5000,
+                stopOnHover: true,
+            });
+        });
+        $(window).resize();
 });
